f841b634da7842ec4b25618610ceeb429fec06eb
taeyoung <pty0220@gmail.com>
2021-01-10 03:26:18 +0900

    small data

--- Pix2Pix3D.py
+++ Pix2Pix3D.py
@@ -1065,7 +1065,7 @@ class CycleGAN():
         def split(lists):
             t = 0
             for x in lists:
-                if t%2 == 0:
+                if t%100 == 0:
                     names = os.path.basename(x)
                 t = t+1
             return names
